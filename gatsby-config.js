4c1edbf08080904402a8e78da55821ad7fdc7b62
Ana Tarrisse <4750042+atarrisse@users.noreply.github.com>
2021-04-10 17:51:34 -0300

    pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,20 @@
 module.exports = {
   plugins: [
-    `gatsby-plugin-image`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-plugin-image",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Enquanto Isso - CTI",
+        short_name: "Enquanto Isso",
+        start_url: "/",
+        background_color: "#DD6B2E",
+        theme_color: "#FACF54",
+        display: "standalone",
+        icon: "static/cat-icon.png",
+      },
+    },
+    "gatsby-plugin-offline",
   ],
 }
